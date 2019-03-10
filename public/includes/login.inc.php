07bcb00533761fd2672c5f528d8f839e50ea4b79
MrRedstoneWolf <stijn@sharks.nl>
2019-03-10 20:15:33 +0100

    3.7 Change links, remove phone login

--- public/includes/login.inc.php
+++ public/includes/login.inc.php
@@ -2,18 +2,18 @@
 session_start();
 if (isset($_POST['submit'])) {
 	include 'dbh.inc.php';
-	$name = mysqli_real_escape_string($conn, $_POST['email']); // email or tel
-	$pwd = mysqli_real_escape_string($conn, $_POST['wachtwoord']);
+	$name = mysqli_real_escape_string($conn, $_POST['email_login']); // email
+	$pwd = mysqli_real_escape_string($conn, $_POST['wachtwoord_login']); // wachtwoord
 	if (empty($name) || empty($pwd)) {
 		header("../login.php?Error=Emtpy");
 		echo 'Please fill in both fields.';
 		exit();
 	} else {		//Lees database
-		$sql = "SELECT * FROM tbl_klanten WHERE email='$name' OR telefoonnummer='$name'"; //ook aanmelden met tel nummer?
+		$sql = "SELECT * FROM tbl_klanten WHERE email='$name'";
 		$result = mysqli_query($conn, $sql);
 		$resultCheck = mysqli_num_rows($result);		//Check of het account bestaat
 		if ($resultCheck < 1) {
-			header("../login.php?Error");
+			header("../signup.php?Error");
 			echo 'Error';
 			exit();
 		} else {
@@ -24,14 +24,14 @@ if (isset($_POST['submit'])) {
 					echo 'Wrong password';
 					exit();
 				} elseif ($hashedPwdCheck == true) {
-					//Inloggen					$_SESSION['c_id'] = $row['!!!'];//bedrijf naam
+					//Inloggen
 					$_SESSION['u_id'] = $row['klantnummer'];//user ID
 					$_SESSION['u_first'] = $row['voornaam'];//voornaam
 					$_SESSION['u_last'] = $row['achternaam'];//achternaam
 					$_SESSION['u_email'] = $row['email'];//email
 					$_SESSION['u_tel'] = $row['telefoonnummer'];//tel
-					$_SESSION['u_com'] = $row['bedrijf'];//tel
-					header("../login.php?Succes");
+					$_SESSION['u_com'] = $row['bedrijf'];//bedrijf
+					header("../signup.php?Succes");
 					exit();
 				}
 			}
