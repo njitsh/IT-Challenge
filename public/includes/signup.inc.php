1a83cc8c291ceae14fd561f9ee79755f2a3269a5
MrRedstoneWolf <stijn@sharks.nl>
2019-03-10 13:42:23 +0100

    3.3 Check if passwords match

--- public/includes/signup.inc.php
+++ public/includes/signup.inc.php
@@ -27,54 +27,58 @@ if (isset($_POST['submit'])) {
 		echo '</script>';
 
 		exit();
-	} else {		//Kijk of wachtwoorden hetzelfde zijn		if($pwd != $pwd2) {			header("Location: ../signup.php?signup=passwordsdontmatch");			exit();		}else {
-			//Kijk of alle karakters zijn toegestaan
-			if (!preg_match("/^[a-zA-Z]*$/", $first) || !preg_match("/^[a-z A-Z]*$/", $last) || !preg_match("/^[a-z A-Z]*$/", $company)) {
-				//header("Location: ../signup.php?signup=invalid");
-
-				echo '<script language="javascript">';
-				echo 'alert("character fail")';
-				echo '</script>';
-
-				exit();
-			} else {
-				//Kijk of email bestaat
-				if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-					//header("Location: ../signup.php?signup=email");
+	} else {		//Kijk of wachtwoorden hetzelfde zijn
+		if($pwd != $pwd2) {			header("Location: ../signup.php?signup=passwordsdontmatch");			exit();
+		}
+		else {
+				//Kijk of alle karakters zijn toegestaan
+				if (!preg_match("/^[a-zA-Z]*$/", $first) || !preg_match("/^[a-z A-Z]*$/", $last) || !preg_match("/^[a-z A-Z]*$/", $company)) {
+					//header("Location: ../signup.php?signup=invalid");
 
 					echo '<script language="javascript">';
-					echo 'alert("email fail")';
+					echo 'alert("character fail")';
 					echo '</script>';
 
 					exit();
-				} else {					//Kijk of het account niet al bestaat
-					$sql = "SELECT * FROM tbl_klanten WHERE email='$email' OR telefoonnummer='$tel'";//max 1 email en 1 tel in database
-					$result = mysqli_query($conn, $sql);
-					$resultCheck = mysqli_num_rows($result);
-					if ($resultCheck > 0) {
-						//header("Location: ../signup.php?signup=alreadyexists");
+				} else {
+					//Kijk of email bestaat
+					if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+						//header("Location: ../signup.php?signup=email");
 
 						echo '<script language="javascript">';
-						echo 'alert("user already exists")';
+						echo 'alert("email fail")';
 						echo '</script>';
 
 						exit();
-					} else {
-						//Wachtwoord hashen
-						$hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
-						//Gebruiker aanmaken
-						$sql = "INSERT INTO tbl_klanten (voornaam, achternaam, email, telefoonnummer, wachtwoord, bedrijf) VALUES ('$first', '$last', '$email', '$tel', '$hashedPwd', '$company');";
-						mysqli_query($conn, $sql);
-						//header("Location: ../signup.php?signup=success");
+					} else {					//Kijk of het account niet al bestaat
+						$sql = "SELECT * FROM tbl_klanten WHERE email='$email' OR telefoonnummer='$tel'";//max 1 email en 1 tel in database
+						$result = mysqli_query($conn, $sql);
+						$resultCheck = mysqli_num_rows($result);
+						if ($resultCheck > 0) {
+							//header("Location: ../signup.php?signup=alreadyexists");
 
-						echo '<script language="javascript">';
-						echo 'alert("success")';
-						echo '</script>';
+							echo '<script language="javascript">';
+							echo 'alert("user already exists")';
+							echo '</script>';
 
-						exit();
+							exit();
+						} else {
+							//Wachtwoord hashen
+							$hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
+							//Gebruiker aanmaken
+							$sql = "INSERT INTO tbl_klanten (voornaam, achternaam, email, telefoonnummer, wachtwoord, bedrijf) VALUES ('$first', '$last', '$email', '$tel', '$hashedPwd', '$company');";
+							mysqli_query($conn, $sql);
+							//header("Location: ../signup.php?signup=success");
+
+							echo '<script language="javascript">';
+							echo 'alert("success")';
+							echo '</script>';
+
+							exit();
+						}
 					}
-				}
-			}
+				}
+			}
 		}
 	} else {
 	//header("Location: ../signup.php");
