5608d3de5822dd8b42665d8d3a75c05dca5ffd5d
MrRedstoneWolf <stijn@sharks.nl>
2019-03-10 14:27:02 +0100

    3.4 Change database password

--- public/includes/dbh.inc.php
+++ public/includes/dbh.inc.php
@@ -1,6 +1,6 @@
 <?php
 $dbServername = "localhost";
 $dbUsername = "root";
-$dbPassword = "";
+$dbPassword = "usbw";
 $dbName = "it-challenge";
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
