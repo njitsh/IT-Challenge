8c34245709328539b7ea8f6c2e881afcff46e09f
MrRedstoneWolf <stijn@sharks.nl>
2019-03-15 23:19:16 +0100

    4.8 Add other pages to make testing easier

---
+++ public/includes/dbh.inc.php
@@ -0,0 +1,1 @@
+<?php$dbServername = "localhost";$dbUsername = "root";$dbPassword = "usbw";$dbName = "it-challenge";$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
---
+++ public/includes/logout.inc.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+
+$_SESSION['u_id'] = NULL;
+$_SESSION['u_first'] = NULL;
+$_SESSION['u_last'] = NULL;
+$_SESSION['u_email'] = NULL;
+$_SESSION['u_tel'] = NULL;
+$_SESSION['u_com'] = NULL;
+
+session_unset();
+session_destroy();
+
+header("Location: ../index.php");
+exit();
+?>
