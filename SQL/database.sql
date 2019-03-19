223b3c640ccbe5a972959b7f18ac79979068dd15
dfgdg <fdg>
2019-03-19 12:10:20 +0100

    Add materialen and belijming table

--- SQL/database.sql
+++ SQL/database.sql
@@ -25,5 +25,16 @@ CREATE TABLE tbl_orders(
     datum_laatst_bewerkt datetime NOT NULL,
     status varchar(255) DEFAULT 'Aangevraagd' NOT NULL,
     opmerking_klant varchar(255) NULL,
-    opmerking_admin varchar(255) NULL
+    opmerking_admin varchar(255) NULL,
+    order bit(1) DEFAULT 0 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE tbl_materialen(
+    materiaalnummer  int(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    materiaal varchar(255) NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE tbl_belijming(
+    belijmingnummerr  int(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    belijming varchar(255) NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
