8d6a3e37ff856d5a63d46f371427557221adce96
MrRedstoneWolf <stijn@sharks.nl>
2019-03-24 12:44:02 +0100

    4.30 Order handling is even better now!

--- public/includes/create_order.inc.php
+++ public/includes/create_order.inc.php
@@ -19,6 +19,46 @@ if (isset($_POST['submit'])) {
 	$oplage = mysqli_real_escape_string($conn, $_POST['oplage']);
 	$opmerking_klant = mysqli_real_escape_string($conn, $_POST['opmerking_klant']);
 
+	//Upload afbeelding
+	$target_dir = "uploads/";
+	$target_file = $target_dir.basename($_FILES["afwerking_afbeelding"]["name"]);
+	$afbeelding_naam = basename($_FILES["afwerking_afbeelding"]["name"]);
+	$uploadOk = 1;
+	$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+	// Check if image file is a actual image or fake image
+	if(isset($_POST["submit"])) {
+	    $check = getimagesize($_FILES["afwerking_afbeelding"]["tmp_name"]);
+	    if($check !== false) {
+	        echo "File is an image - " . $check["mime"] . ".";
+	        $uploadOk = 1;
+	    } else {
+	        echo "File is not an image.";
+	        $uploadOk = 0;
+	    }
+	}
+	// Check file size
+	if ($_FILES["afwerking_afbeelding"]["size"] > 500000) {
+	    echo "Sorry, your file is too large.";
+	    $uploadOk = 0;
+	}
+	// Allow certain file formats
+	if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+	&& $imageFileType != "gif" ) {
+	    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+	    $uploadOk = 0;
+	}
+	// Check if $uploadOk is set to 0 by an error
+	if ($uploadOk == 0) {
+	    echo "Sorry, your file was not uploaded.";
+	// if everything is ok, try to upload file
+	} else {
+	    if (move_uploaded_file($_FILES["afwerking_afbeelding"]["tmp_name"], $target_file)) {
+	        echo "The file ". basename( $_FILES["afwerking_afbeelding"]["name"]). " has been uploaded.";
+	    } else {
+	        echo "Sorry, there was an error uploading your file.";
+	    }
+	}
+
 	//Kijk of iets leeg is
 	if (empty($breedte) || empty($hoogte) || empty($radius) || empty($tussenafstand) || empty($rolbreedte) || empty($materiaal) || empty($bedrukking) || empty($afwerking) || empty($wikkeling) || empty($oplage)) {
 		//header("Location: ../signup?signup=empty");
@@ -50,7 +90,7 @@ if (isset($_POST['submit'])) {
 			} else {
 
 						//Order aanmaken
-						$sql = "INSERT INTO tbl_orders (klantnummer, breedte, hoogte, radius, tussenafstand, rolbreedte, materiaal, bedrukking, afwerking, wikkeling, oplage, datum_aangemaakt, datum_laatst_bewerkt, opmerking_klant) VALUES ('$user_id', '$breedte', '$hoogte', '$radius', '$tussenafstand', '$rolbreedte', '$materiaal', '$bedrukking', '$afwerking', '$wikkeling', '$oplage', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '$opmerking_klant');";
+						$sql = "INSERT INTO tbl_orders (klantnummer, breedte, hoogte, radius, tussenafstand, rolbreedte, materiaal, bedrukking, afwerking, wikkeling, oplage, datum_aangemaakt, datum_laatst_bewerkt, opmerking_klant, afbeelding_path) VALUES ('$user_id', '$breedte', '$hoogte', '$radius', '$tussenafstand', '$rolbreedte', '$materiaal', '$bedrukking', '$afwerking', '$wikkeling', '$oplage', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '$opmerking_klant', '$afbeelding_naam');";
 						mysqli_query($conn, $sql);
 						$_SESSION['o_id'] = $conn->insert_id;
 						header("Location: send_mail.inc.php");
