fc9b34e613b573a2b237f41c96ffa7350952272e
MrRedstoneWolf <stijn@sharks.nl>
2019-03-24 17:51:25 +0100

    3.18 Add styling

--- public/signup.php
+++ public/signup.php
@@ -27,7 +27,7 @@
 
   <div class="container">
 
-    <a class="logo" href="index"><img src="images/pentolabel.png"></a>
+    <a class="logo" href="signup"><img src="images/pentolabel.png"></a>
 
     <nav>
       <ul>
@@ -37,25 +37,37 @@
 
     <?php if (isset($_SESSION['u_id']))
     {
-      header("Location: index");
+      header("Location: order");
     } else { ?>
-      <h4><strong>Inloggen</strong></h4>
+      <h4><strong>Inloggen/Registreren</strong></h4>
       <form class="inloggen" action="includes/login.inc" method="POST" autocomplete="on">
-          <input type="email" name="email_login" placeholder="Voer hier uw e-mailadres in*" required autofocus pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" title="Voer een geldig e-mailadres in"> <br>
-          <input type="password" name="wachtwoord_login" placeholder="Voer hier uw wachtwoord in" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Moet minimaal een nummer, een hoofdletter en kleine letter bevatten en uit minimaal 8 tekens bestaan"><br>
-          <input id="submit" type="submit" name="submit" value="submit">
+        <h6><strong>Inloggen</strong></h6>
+        <div style="display: grid; grid-column-gap: 10px; grid-template-columns: 1fr 1fr;">
+          <div><strong>E-mailadres</strong><input type="email" name="email_login" placeholder="Voer hier uw e-mailadres in*" required autofocus pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" title="Voer een geldig e-mailadres in"><br></div>
+          <div><strong>Wachtwoord</strong><input type="password" name="wachtwoord_login" placeholder="Voer hier uw wachtwoord in" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Moet minimaal een nummer, een hoofdletter en kleine letter bevatten en uit minimaal 8 tekens bestaan"><br></div>
+        </div>
+        <input id="submit" style="margin-top: 10px; margin-bottom: 5px;" type="submit" name="submit" value="Inloggen">
       </form>
 
-      <h4><strong>Registreren</strong></h4>
       <form class="registreren" action="includes/signup.inc" method="POST" autocomplete="off">
-          <input type="email" name="email_registreren" placeholder="Voer hier uw e-mailadres in*" required autofocus pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" title="Voer een geldig e-mailadres in"><br>
-          <input type="text" name="voornaam_registreren" placeholder="Voer hier uw voornaam in*" required minlength="3" title="Voer hier uw voornaam in"><br>
-          <input type="text" name="achternaam_registreren" placeholder="Voer hier uw achternaam in*" required minlength="3" title="Voer hier uw achternaam in"><br>
-          <input type="tel"  name="telefoon_registreren" placeholder="Voer hier uw telefoonnummer in*" required pattern="(^\+[0-9]{2}|^\+[0-9]{2}\(0\)|^\(\+[0-9]{2}\)\(0\)|^00[0-9]{2}|^0)([0-9]{9}$|[0-9\-\s]{10}$)" title="Voer hier uw telefoonnummer in"><br>
-          <input type="text" name="bedrijf_registreren" placeholder="Voer hier de naam van uw bedrijf in" title="Voer hier de naam van uw bedrijf in"><br>
-          <input type="password" name="wachtwoord_registreren" placeholder="Voer hier uw wachtwoord in*" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Moet minimaal een nummer, een hoofdletter en kleine letter bevatten en uit minimaal 8 tekens bestaan"><br>
-          <input type="password" name="wachtwoord_2_registreren" placeholder="Bevestig uw wachtwoord*" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Moet hetzelfde zijn als uw eerste wachtwoord"><br>
-          <input id="submit" type="submit" name="submit" value="submit">
+        <h6><strong>Registreren</strong></h6>
+        <div style="display: grid; grid-template-columns: 1fr 1fr; grid-column-gap: 10px; margin-bottom: 10px;">
+          <div><strong>E-mailadres*</strong><input type="email" name="email_registreren" placeholder="Voer hier uw e-mailadres in*" required autofocus pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" title="Voer een geldig e-mailadres in"><br></div>
+          <div><strong>Telefoonnummer*</strong><input type="tel"  name="telefoon_registreren" placeholder="Voer hier uw telefoonnummer in*" required pattern="(^\+[0-9]{2}|^\+[0-9]{2}\(0\)|^\(\+[0-9]{2}\)\(0\)|^00[0-9]{2}|^0)([0-9]{9}$|[0-9\-\s]{10}$)" title="Voer hier uw telefoonnummer in"><br></div>
+        </div>
+        <div style="display: grid; grid-template-columns: 1fr 1fr; grid-column-gap: 10px; margin-bottom: 10px;">
+          <div><strong>Voornaam*</strong><input type="text" name="voornaam_registreren" placeholder="Voer hier uw voornaam in*" required minlength="3" title="Voer hier uw voornaam in"><br></div>
+          <div><strong>Achternaam*</strong><input type="text" name="achternaam_registreren" placeholder="Voer hier uw achternaam in*" required minlength="3" title="Voer hier uw achternaam in"><br></div>
+        </div>
+        <div style="display: grid; grid-template-columns: 1fr 1fr; grid-column-gap: 10px; margin-bottom: 10px;">
+          <div><strong>Bedrijf</strong><input type="text" name="bedrijf_registreren" placeholder="Voer hier de naam van uw bedrijf in" title="Voer hier de naam van uw bedrijf in"><br></div>
+          <div><strong>Adres*</strong><input type="text" name="adres_registreren" placeholder="Voer hier uw adres in" title="Voer hier uw adres in"><br></div>
+        </div>
+        <div style="display: grid; grid-template-columns: 1fr 1fr; grid-column-gap: 10px; margin-bottom: 10px;">
+          <div><strong>Wachtwoord*</strong><input type="password" name="wachtwoord_registreren" placeholder="Voer hier uw wachtwoord in*" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Moet minimaal een nummer, een hoofdletter en kleine letter bevatten en uit minimaal 8 tekens bestaan"><br></div>
+          <div><strong>Wachtwoord bevestigen*</strong><input type="password" name="wachtwoord_2_registreren" placeholder="Bevestig uw wachtwoord*" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Moet hetzelfde zijn als uw eerste wachtwoord"><br></div>
+        </div>
+          <input id="submit" style="margin-bottom: 5px;" type="submit" name="submit" value="Registreren">
       </form>
 
     <?php } ?>
